744465e64cd6b438d3bd2dce2004d8ef6202f248
Colm Tuite <colmtuite@gmail.com>
2017-06-23 08:49:56 +0100

    Rough Avatar component

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,5 +1,64 @@
 $(function() {
+
+    // Render navigation content into SideDrawer
+    
     $.get("../partials/navigation.html", function(data){
-        $('#navigation').html(data);
+        $('.js-Navigation').html(data);
+    });
+
+    // Toggle overlay
+
+    function toggleOverlay() {
+        $('.js-Overlay').toggleClass('active');
+    }
+
+    // Toggle SideDrawer
+
+    function toggleSideDrawer() {
+        $('.js-SideDrawer').toggleClass('active');
+        toggleOverlay();
+    }
+
+    // Open SideDrawer
+
+    $('.js-SideDrawerTrigger').click(function() {
+        toggleSideDrawer();
+    });
+
+    // Deactivate components when click overlay
+
+    $('.js-Overlay').click(function() {
+        toggleSideDrawer();
+        console.log('Fuck off');
+    });
+
+    // Prevent scroll bubbling
+
+    $(document).on('DOMMouseScroll mousewheel', '.Scrollbar', function(ev) {
+        var $this = $(this),
+            scrollTop = this.scrollTop,
+            scrollHeight = this.scrollHeight,
+            height = $this.innerHeight(),
+            delta = (ev.type == 'DOMMouseScroll' ?
+                ev.originalEvent.detail * -40 :
+                ev.originalEvent.wheelDelta),
+            up = delta > 0;
+
+        var prevent = function() {
+            ev.stopPropagation();
+            ev.preventDefault();
+            ev.returnValue = false;
+            return false;
+        }
+
+        if (!up && -delta > scrollHeight - height - scrollTop) {
+            // Scrolling down, but this will take us past the bottom.
+            $this.scrollTop(scrollHeight);
+            return prevent();
+        } else if (up && delta > scrollTop) {
+            // Scrolling up, but this will take us past the top.
+            $this.scrollTop(0);
+            return prevent();
+        }
     });
 });

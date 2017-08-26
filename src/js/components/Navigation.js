d88378ae0f2ff312284709b61e16b4507c68131c
Roman Milkovic <destabiliseus@gmail.com>
2017-08-26 17:19:57 +0800

    Build Sidedrawer js component

--- src/js/components/Navigation.js
+++ src/js/components/Navigation.js
@@ -1,57 +1,57 @@
-$(function() {
+// $(function() {
 
-    // Toggle overlay
+//     // Toggle overlay
 
-    function toggleOverlay() {
-        $('.js-Overlay').toggleClass('active');
-    }
+//     function toggleOverlay() {
+//         $('.js-Overlay').toggleClass('active');
+//     }
 
-    // Toggle SideDrawer
+//     // Toggle SideDrawer
 
-    function toggleSideDrawer() {
-        $('.js-SideDrawer').toggleClass('active');
-        toggleOverlay();
-    }
+//     function toggleSideDrawer() {
+//         $('.js-SideDrawer').toggleClass('active');
+//         toggleOverlay();
+//     }
 
-    // Open SideDrawer
+//     // Open SideDrawer
 
-    $('.js-SideDrawerTrigger').click(function() {
-        toggleSideDrawer();
-    });
+//     $('.js-SideDrawerTrigger').click(function() {
+//         toggleSideDrawer();
+//     });
 
-    // Deactivate components when click overlay
+//     // Deactivate components when click overlay
 
-    $('.js-Overlay').click(function() {
-        toggleSideDrawer();
-    });
+//     $('.js-Overlay').click(function() {
+//         toggleSideDrawer();
+//     });
 
-    // Prevent scroll bubbling
+//     // Prevent scroll bubbling
 
-    $(document).on('DOMMouseScroll mousewheel', '.js-preventScrollBubbling', function(ev) {
-        var $this = $(this),
-            scrollTop = this.scrollTop,
-            scrollHeight = this.scrollHeight,
-            height = $this.innerHeight(),
-            delta = (ev.type == 'DOMMouseScroll' ?
-                ev.originalEvent.detail * -40 :
-                ev.originalEvent.wheelDelta),
-            up = delta > 0;
+//     $(document).on('DOMMouseScroll mousewheel', '.js-preventScrollBubbling', function(ev) {
+//         var $this = $(this),
+//             scrollTop = this.scrollTop,
+//             scrollHeight = this.scrollHeight,
+//             height = $this.innerHeight(),
+//             delta = (ev.type == 'DOMMouseScroll' ?
+//                 ev.originalEvent.detail * -40 :
+//                 ev.originalEvent.wheelDelta),
+//             up = delta > 0;
 
-        var prevent = function() {
-            ev.stopPropagation();
-            ev.preventDefault();
-            ev.returnValue = false;
-            return false;
-        }
+//         var prevent = function() {
+//             ev.stopPropagation();
+//             ev.preventDefault();
+//             ev.returnValue = false;
+//             return false;
+//         }
 
-        if (!up && -delta > scrollHeight - height - scrollTop) {
-            // Scrolling down, but this will take us past the bottom.
-            $this.scrollTop(scrollHeight);
-            return prevent();
-        } else if (up && delta > scrollTop) {
-            // Scrolling up, but this will take us past the top.
-            $this.scrollTop(0);
-            return prevent();
-        }
-    });
-});
+//         if (!up && -delta > scrollHeight - height - scrollTop) {
+//             // Scrolling down, but this will take us past the bottom.
+//             $this.scrollTop(scrollHeight);
+//             return prevent();
+//         } else if (up && delta > scrollTop) {
+//             // Scrolling up, but this will take us past the top.
+//             $this.scrollTop(0);
+//             return prevent();
+//         }
+//     });
+// });

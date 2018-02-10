a114b18fd4a2e670b5cff91058084fb1653a74fd
Colm Tuite <colmtuite@gmail.com>
2018-02-10 15:42:57 +0000

    Why oh why must I refuse to use system fonts ffs

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -55,7 +55,24 @@ const config = {
                         }
                     }
                 ]
-            }
+            },
+            {
+                // Match woff2 in addition to patterns like .woff?v=1.1.1.
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader",
+                options: {
+                    // Limit at 50k. Above that it emits separate files
+                    limit: 50000,
+
+                    // url-loader sets mimetype if it's passed.
+                    // Without this it derives it from the file extension
+                    mimetype: "application/font-woff",
+
+                    // Output below fonts directory
+                    name: "./fonts/[name].[ext]",
+                    publicPath: "../", // Take the directory into account
+                },
+            },
         ]
     },
     devServer: {
